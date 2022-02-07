2514234e152b18b99a2828774761b8607879adee
tom-stringer <tomstringer99@gmail.com>
2022-02-07 17:33:50 +0000

    Add Tailwind CSS

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { RecoilRoot } from "recoil";
 import App from "./components/App";
+import "./styles/output.css";
 
 ReactDOM.render(
     <React.StrictMode>
