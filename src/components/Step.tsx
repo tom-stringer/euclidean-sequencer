dd120b4d1c5d348ba17502c0cc5360f4d21aec02
tom-stringer <tomstringer99@gmail.com>
2022-02-17 12:18:58 +0000

    Replace off-step indicator with small circle

--- src/components/Step.tsx
+++ src/components/Step.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import { FC } from "react";
 import { Track } from "../types/rhythm-types";
 
@@ -11,8 +12,7 @@ const Step: FC<StepProps> = ({ track, radius, index }) => {
     const active = track.necklace[index];
     const offsetAngle = 360 / track.steps;
     const rotateAngle = offsetAngle * index;
-    const width = active ? 20 : 5;
-    const height = active ? width : 10;
+    const diameter = active ? 20 : 10;
 
     /*
         Rotate step to face its position, based on its index. Then move it forward, and rotate back.
@@ -22,28 +22,17 @@ const Step: FC<StepProps> = ({ track, radius, index }) => {
         transform: active
             ? `rotate(${rotateAngle}deg) translate(0, -${radius}px) rotate(-${rotateAngle}deg)`
             : `rotate(${rotateAngle}deg) translate(0, -${radius}px)`,
-        margin: active ? `-${width / 2}px` : "",
-        width: `${width}px`,
-        height: `${height}px`,
+        margin: `-${diameter / 2}px`,
+        width: `${diameter}px`,
+        height: `${diameter}px`,
     };
 
-    let className = `absolute top-1/2 left-1/2 `;
+    const className = classNames("rounded-full absolute top top-1/2 left-1/2", {
+        "bg-orange-light": active,
+        "bg-orange-dark": !active,
+    });
 
-    if (active) {
-        className += "bg-orange-light rounded-full";
-    } else {
-        className += "stroke-orange-dark";
-    }
-
-    if (active) {
-        return <div key={index} className={className} style={style} />;
-    } else {
-        return (
-            <svg className={className} style={style} viewBox="0 0 8 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <line x1={4} y1={0} x2={4} y2={16} stroke-width="4" />
-            </svg>
-        );
-    }
+    return <div key={index} className={className} style={style} />;
 };
 
 export default Step;
